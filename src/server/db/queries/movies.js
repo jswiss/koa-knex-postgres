4c6a3a7e0e5bbe28ff66238c6bf4980888ef8aa9
Joshua Swiss <swissjoshua@gmail.com>
2018-12-21 13:47:36 +0000

    getSingleMovie test passing

--- src/server/db/queries/movies.js
+++ src/server/db/queries/movies.js
@@ -4,6 +4,13 @@ function getAllMovies() {
 	return knex('movies').select('*');
 }
 
+function getSingleMovie(id) {
+	return knex('movies')
+		.select('*')
+		.where({ id: parseInt(id) });
+}
+
 module.exports = {
 	getAllMovies,
+	getSingleMovie,
 };
--- src/server/routes/movie.js
+++ src/server/routes/movie.js
@@ -4,12 +4,26 @@ const queries = require('../db/queries/movies');
 const router = new Router();
 const BASE_URL = `/api/v1/movies`;
 
+// all movies
 router.get(BASE_URL, async ctx => {
 	try {
 		const movies = await queries.getAllMovies();
 		ctx.body = {
 			status: 'success',
 			data: movies,
+		};
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+// one movie
+router.get(`${BASE_URL}/:id`, async ctx => {
+	try {
+		const movie = await queries.getSingleMovie(ctx.params.id);
+		ctx.body = {
+			status: 'success',
+			data: movie,
 		};
 	} catch (err) {
 		console.log(err);
