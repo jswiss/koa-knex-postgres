666dfcc440525191b18634c1e544ccfa6e201726
Joshua Swiss <swissjoshua@gmail.com>
2018-12-21 14:09:50 +0000

    put test working

--- src/server/db/queries/movies.js
+++ src/server/db/queries/movies.js
@@ -10,7 +10,22 @@ function getSingleMovie(id) {
 		.where({ id: parseInt(id) });
 }
 
+function addMovie(movie) {
+	return knex('movies')
+		.insert(movie)
+		.returning('*');
+}
+
+function updateMovie(id, movie) {
+	return knex('movies')
+		.update(movie)
+		.where({ id: parseInt(id) })
+		.returning('*');
+}
+
 module.exports = {
 	getAllMovies,
 	getSingleMovie,
+	addMovie,
+	updateMovie,
 };
--- test/routes.movies.test.js
+++ test/routes.movies.test.js
@@ -104,4 +104,125 @@ describe('routes : movies', () => {
 				});
 		});
 	});
+
+	describe('POST /api/v1/movies', () => {
+		it('should return the movie that was added', done => {
+			chai
+				.request(server)
+				.post('/api/v1/movies')
+				.send({
+					name: 'Titanic',
+					genre: 'Drama',
+					rating: 8,
+					explicit: true,
+				})
+				.end((err, res) => {
+					// there should be no errors
+					should.not.exist(err);
+					// there should be a 201 status code
+					// (indicating that something was "created")
+					res.status.should.equal(201);
+					// the response should be JSON
+					res.type.should.equal('application/json');
+					// the JSON response body should have a
+					// key-value pair of {"status": "success"}
+					res.body.status.should.eql('success');
+					// the JSON response body should have a
+					// key-value pair of {"data": 1 movie object}
+					res.body.data[0].should.include.keys(
+						'id',
+						'name',
+						'genre',
+						'rating',
+						'explicit'
+					);
+					done();
+				});
+		});
+		it('should throw an error if the payload is malformed', done => {
+			chai
+				.request(server)
+				.post('/api/v1/movies')
+				.send({
+					name: 'Titanic',
+				})
+				.end((err, res) => {
+					// there should an error
+					should.exist(err);
+					// there should be a 400 status code
+					res.status.should.equal(400);
+					// the response should be JSON
+					res.type.should.equal('application/json');
+					// the JSON response body should have a
+					// key-value pair of {"status": "error"}
+					res.body.status.should.eql('error');
+					// the JSON response body should have a message key
+					should.exist(res.body.message);
+					done();
+				});
+		});
+	});
+
+	describe('PUT /api/v1/movies', () => {
+		it('should return the movie that was updated', done => {
+			knex('movies')
+				.select('*')
+				.then(movie => {
+					const movieObject = movie[0];
+					chai
+						.request(server)
+						.put(`/api/v1/movies/${movieObject.id}`)
+						.send({
+							rating: 9,
+						})
+						.end((err, res) => {
+							// there should be no errors
+							should.not.exist(err);
+							// there should be a 200 status code
+							res.status.should.equal(200);
+							// the response should be JSON
+							res.type.should.equal('application/json');
+							// the JSON response body should have a
+							// key-value pair of {"status": "success"}
+							res.body.status.should.eql('success');
+							// the JSON response body should have a
+							// key-value pair of {"data": 1 movie object}
+							res.body.data[0].should.include.keys(
+								'id',
+								'name',
+								'genre',
+								'rating',
+								'explicit'
+							);
+							// ensure the movie was in fact updated
+							const newMovieObject = res.body.data[0];
+							newMovieObject.rating.should.not.eql(movieObject.rating);
+							done();
+						});
+				});
+		});
+		it('should throw an error if the movie does not exist', done => {
+			chai
+				.request(server)
+				.put('/api/v1/movies/9999999')
+				.send({
+					rating: 9,
+				})
+				.end((err, res) => {
+					// there should an error
+					should.exist(err);
+					// there should be a 404 status code
+					res.status.should.equal(404);
+					// the response should be JSON
+					res.type.should.equal('application/json');
+					// the JSON response body should have a
+					// key-value pair of {"status": "error"}
+					res.body.status.should.eql('error');
+					// the JSON response body should have a
+					// key-value pair of {"message": "That movie does not exist."}
+					res.body.message.should.eql('That movie does not exist.');
+					done();
+				});
+		});
+	});
 });
